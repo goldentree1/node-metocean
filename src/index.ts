94cc8a310889eceb298b3b0c8999f7109f00dad2
Elliott Brown <105253089+goldentree1@users.noreply.github.com>
2023-05-30 21:21:50 +1200

    make smaller

--- src/index.ts
+++ src/index.ts
@@ -212,27 +212,45 @@ export type MetOceanFetchOptions = Omit<RequestInit, 'body' | 'headers' | 'metho
 
 /** MetOcean errors extend this class.*/
 export abstract class MetOceanError extends Error {
-    constructor(message: string) { super(message); }
-}
-/** Error indicating that an illegal argument has been provided. */
-export class MetOceanIllegalArgumentError extends MetOceanError {
-    constructor(message: string) {
-        super(`MetOcean Illegal Argument Error (no API requests were made):\n${message}`);
-    }
-}
-/** Error indicating that there was an issue with the API request response. */
-export class MetOceanRequestError extends MetOceanError {
     private _httpStatusCode;
+    private _errorList;
 
-    constructor({ message, httpStatusCode }: { message: string; httpStatusCode: number; }) {
-        super(`MetOcean Response Error:\nStatus ${httpStatusCode}\n${message}`);
+    constructor(errorList: string[], httpStatusCode: number, errType?: string) {
+        super(`MetOceanError: \n${errorList.map(msg => `\t - ${msg}`)}\nHTTP Status code: ${httpStatusCode} ${errType ? `(${errType})` : ``}`);
         this._httpStatusCode = httpStatusCode;
+        this._errorList = errorList;
     }
-
-    get httpStatusCode() {
+    public get httpStatusCode() {
         return this._httpStatusCode;
     }
+    public get errorList() {
+        return this._errorList;
+    }
 }
+/** Error indicating that the user is unauthorized: usually the result of an invalid `apiKey`. */
+export class MetOceanUnauthorizedError extends MetOceanError {
+    constructor(errorList: string[]) {
+        super(errorList, 401, 'unauthorized')
+    }
+}
+/** Error indicating that the resource was not found. */
+export class MetOceanNotFoundError extends MetOceanError {
+    constructor(errorList: string[]) {
+        super(errorList, 404, 'unauthorized')
+    }
+}
+/** Error indicating that the input is invalid. For example, specifying invalid parameters. */
+export class MetOceanInputError extends MetOceanError {
+    constructor(errorList: string[], httpStatusCode: number) {
+        super(errorList, httpStatusCode, 'input error') //some other 4xx code
+    }
+}
+export class MetOceanServerError extends MetOceanError {
+    constructor(errorList: string[], httpStatusCode: number) {
+        super(errorList, httpStatusCode, 'server error')
+    }
+}
+
 /**
  * Class used to interact with the MetOcean API. Requires an `apiKey`.
  * @see https://forecast-docs.metoceanapi.com/docs/#/getting-started
@@ -240,88 +258,39 @@ export class MetOceanRequestError extends MetOceanError {
  */
 export class MetOcean {
 
-    private static _POINT_TIME_SERIES_BASE_URL = 'https://forecast-v2.metoceanapi.com/point/time';
-    private static _ROUTE_TIME_SERIES_BASE_URL = 'https://forecast-v2.metoceanapi.com/route/time';
-    private static _ROUTE_SPEED_BASE_URL = 'https://forecast-v2.metoceanapi.com/route/speed';
-    private static _POINT_BASE_URL = 'https://forecast-v2.metoceanapi.com/point';
-
-    /**Checks that at least one of `times` and `time` were included, but not both */
-    private static _THROW_IF_TIME_AND_TIMES_OR_NEITHER({ time, times }: Pick<MetOceanPointTimeSeriesArgs<any>, 'time' | 'times'>) {
-        if (time && times) throw new MetOceanIllegalArgumentError(`Both 'time' and 'times' properties were included as arguments, but only one of the two can be included. Please remove one.`);
-        if (!time && !times) throw new MetOceanIllegalArgumentError(`Neither 'time' or 'times' was included. Please include one of these properties.`)
-    }
-
-    /**Checks that time variable is ok (if it exists) */
-    private static _THROW_IF_TIME_INVALID({ time }: Pick<MetOceanPointTimeSeriesArgs<any>, 'time'>) {
-        if (!time) return;
-        const { from, repeat, to, interval } = time;
-        //Don't allow simultaneous 'to' and 'repeat' properties
-        if (repeat !== undefined && to !== undefined) throw new MetOceanIllegalArgumentError(`Both 'repeat' and 'to' properties were included in 'time', but only one of the two can be included. Please remove one.`);
-
-        //At least one of 'from' and 'to' must exist.
-        if (from === undefined && to === undefined) throw new MetOceanIllegalArgumentError(`Either 'from' or 'to' properties are required in property 'time'. Please add at least one.`);
-
-        //check types are correct if they do exist.
-        if (from && !(from instanceof Date)) throw new MetOceanIllegalArgumentError(`Property 'from' in property 'time' must be a Date or undefined. Got ${typeof from} ${typeof from === 'string' || typeof from === "number" ? `: ": '${from}'"` : '.'}`);
-        if (to && !(to instanceof Date)) throw new MetOceanIllegalArgumentError(`Property 'to' in property 'time' must be a Date or undefined. Got ${typeof to} ${typeof to === 'string' || typeof to === "number" ? `: ": '${to}'"` : '.'}`)
-        if (interval && typeof interval !== 'number') throw new MetOceanIllegalArgumentError(`Property 'interval' in property 'time' must be a number or undefined. Got ${typeof interval} ${typeof interval === 'string' || typeof interval === "number" ? `: "${interval}"` : ''}`)
-        if (repeat && typeof repeat !== 'number') throw new MetOceanIllegalArgumentError(`Property 'repeat' in property 'time' must be a number or undefined. Got ${typeof repeat} ${typeof repeat === 'string' || typeof repeat === "number" ? `: "${repeat}"` : ''}`)
-    }
-
-    /**Checks that times is an array of Date objects w/ length > 0 (if it exists) */
-    private static _THROW_IF_TIMES_INVALID({ times }: { times: unknown }) {
-        if (!times) return;
-        if (!Array.isArray(times)) throw new MetOceanIllegalArgumentError(`'times' property must be an array or undefined`)
-        if (times.length === 0) throw new MetOceanIllegalArgumentError(`'times' property must contain an array of at least one Date(s), or be undefined. It was an empty array.`);
-        times.map((t, i) => { if (!(t instanceof Date)) throw new MetOceanIllegalArgumentError(`'times' property must contain an array of only Date objects, but ${typeof t} was found at index ${i} ${typeof t === 'string' ? `: "${t}"` : ''}`) });
-    }
-
-    /** Checks that variables are an array of strings only */
-    private static _THROW_IF_VARIABLES_BAD({ variables }: { variables: unknown }) {
-        //throw if variables dont exist, if not an array, or if array is empty
-        if (!variables || !Array.isArray(variables) || variables.length < 1) throw new MetOceanIllegalArgumentError(`'variables' property must contain an array of at least one forecast variable e.g., ["wave.height"]`);
-        variables.forEach((v) => {
-            if (typeof v !== 'string') throw new MetOceanIllegalArgumentError(`Variables array must all be of type 'string', but found ${typeof v}: "${typeof v === "string" ? `${v}` : ''}"`)
-        });
-    }
-
-    /**Checks that points in an array of valid coordinates */
-    private static _THROW_IF_POINTS_BAD({ points }: { points: unknown }) {
-        if (!Array.isArray(points)) throw new MetOceanIllegalArgumentError(`'points' property was ${typeof points} instead of an array of points. Try something like 'points: [{lat:-37.82, lon:174.89}]'`)
-        if (points.length === 0) throw new MetOceanIllegalArgumentError(`'points' array contained no points. Try something like 'points: [{lat:-37.82, lon:174.89}]'`)
-        points.forEach(({ lat, lon }) => {
-            const isLatValid = typeof lat === 'number' && lat <= 90 && lat >= -90;
-            const isLonValid = typeof lon === 'number' && lon <= 180 && lon >= -180;
-            if (!isLatValid || !isLonValid) {
-                const errorMsg = `${isLatValid ? '' : ` - Latitude of ${lat} is invalid (latitudes must be between 90 and -90)`} ${isLonValid ? '' : `\n - Longitude of ${lon} is invalid (longitude must be between 180 and -180)`}`
-                throw new MetOceanIllegalArgumentError(errorMsg);
-            }
-        });
-    }
-
-    private static _THROW_IF_ROUTE_BAD({ route }: { route: unknown }) {
-        if (!Array.isArray(route)) {
-            throw new MetOceanIllegalArgumentError(`'route' property was of type ${typeof route} when it should have been an array. Try something like 'route: [{lon: 186.77271306302663, lat: -30.939924331023455, time: '2023-05-30T00:00:00Z'}]' `)
-        }
-        if (route.length === 0) {
-            throw new MetOceanIllegalArgumentError(`'route' array was of length 0.`)
-        }
-    }
-
-    private static _THROW_IF_SPEEDS_BAD({ speeds }: { speeds: unknown }) {
-        if (!Array.isArray(speeds)) {
-            throw new MetOceanIllegalArgumentError(`'speeds' property was of type ${typeof speeds} when it should have been an array. Try something like 'speeds: [10.2, 12.4, 12]' `)
-        }
-    }
+    private static POINT_TIME_SERIES_BASE_URL = 'https://forecast-v2.metoceanapi.com/point/time';
+    private static ROUTE_TIME_SERIES_BASE_URL = 'https://forecast-v2.metoceanapi.com/route/time';
+    private static ROUTE_SPEED_BASE_URL = 'https://forecast-v2.metoceanapi.com/route/speed';
+    private static POINT_BASE_URL = 'https://forecast-v2.metoceanapi.com/point';
 
     private _requestHeaders;
-    private _options;
 
-    constructor({ apiKey, options }: MetOceanConstructor) {
+    constructor({ apiKey }: MetOceanConstructor) {
         this._requestHeaders = { 'Content-Type': 'application/json', 'x-api-key': apiKey }
+    }
 
-        this._options = {
-            validateArgs: options?.validateArgs !== undefined ? options.validateArgs : true, //defaults to true
+    private async _makeRequest(
+        url: string,
+        method: 'post' | 'get',
+        data: { [id: string]: any },
+        fetchOpts: MetOceanFetchOptions = {}
+    ) {
+        const reqOptions: RequestInit = {
+            ...fetchOpts,
+            method,
+            headers: this._requestHeaders,
+            body: JSON.stringify(data),
+        };
+        try {
+            const res: Response = await fetch(url, reqOptions)
+            const json: unknown = await res.json();
+            if (res.status === 200) return json as unknown;
+            if (res.status === 401) throw new MetOceanUnauthorizedError(json as string[]);
+            if (res.status === 404) throw new MetOceanNotFoundError(json as string[]);
+            if (res.status >= 400 && res.status < 500) throw new MetOceanInputError(json as string[], res.status);
+            if (res.status >= 500) throw new MetOceanServerError(json as string[], res.status);
+        } catch (err) {
+            throw err;
         }
     }
 
@@ -342,50 +311,18 @@ export class MetOcean {
      * })
      */
     async getPointTimeSeries<T extends MetOceanTimeSeriesVariable[]>(
-        { points, time, times, variables }: MetOceanPointTimeSeriesArgs<T>,
-        fetchOptions?: MetOceanFetchOptions): Promise<MetOceanPointTimeSeriesResponse<T>> {
+        args: MetOceanPointTimeSeriesArgs<T>,
+        fetchOptions?: MetOceanFetchOptions
+    ): Promise<MetOceanPointTimeSeriesResponse<T>> {
 
-        //validate
-        if (this._options.validateArgs) {
-            MetOcean._THROW_IF_POINTS_BAD({ points });
-            MetOcean._THROW_IF_TIME_AND_TIMES_OR_NEITHER({ time, times });
-            MetOcean._THROW_IF_TIME_INVALID({ time });
-            MetOcean._THROW_IF_TIMES_INVALID({ times });
-            MetOcean._THROW_IF_VARIABLES_BAD({ variables });
-        }
+        const url = MetOcean.POINT_TIME_SERIES_BASE_URL;
+        const res: any = await this._makeRequest(url, "post", args, fetchOptions);
 
-        //build request
-        const url = MetOcean._POINT_TIME_SERIES_BASE_URL;
-        const reqOptions: RequestInit = {
-            ...fetchOptions,
-            method: 'post',
-            headers: this._requestHeaders,
-            body: JSON.stringify({
-                points,
-                time: time ? {
-                    ...time,
-                    interval: time?.interval ? `${time?.interval}h` : undefined,
-                } : undefined,
-                times,
-                variables,
-            }),
-        };
+        //convert strings to JS Dates
+        res.dimensions.time.data = res.dimensions.time.data
+            .map((dateString: string) => new Date(dateString));
 
-        //fetch
-        const res = await fetch(url, reqOptions);
-        const resText = await res.text();
-
-        //Throw request error on bad status code
-        if (res.status !== 200) throw new MetOceanRequestError({
-            httpStatusCode: res.status,
-            message: resText
-        });
-
-        /** @todo improve typing here? */
-        const data = JSON.parse(resText) as any;
-        //Map strings to date array
-        data.dimensions.time.data = data.dimensions.time.data.map((dateString: string) => new Date(dateString));
-        return data as MetOceanPointTimeSeriesResponse<T>;
+        return res as MetOceanPointTimeSeriesResponse<T>;
     }
 
     /**
@@ -407,37 +344,13 @@ export class MetOcean {
      * })   
     */
     async getPoint<T extends MetOceantNonTimeSeriesVariable[]>(
-        { points, variables }: MetOceanPointArgs<T>,
+        args: MetOceanPointArgs<T>,
         fetchOptions?: MetOceanFetchOptions
     ): Promise<MetOceanPointResponse<T>> {
 
-        if (this._options.validateArgs) {
-            MetOcean._THROW_IF_POINTS_BAD({ points });
-            MetOcean._THROW_IF_VARIABLES_BAD({ variables });
-        }
-
-        //build request
-        const url = MetOcean._POINT_BASE_URL;
-        const reqOptions: RequestInit = {
-            ...fetchOptions,
-            method: 'post',
-            headers: this._requestHeaders,
-            body: JSON.stringify({ points, variables }),
-        };
-
-        //fetch
-        const res = await fetch(url, reqOptions);
-        const resText = await res.text();
-
-        //Throw request error on bad status code
-        if (res.status !== 200) throw new MetOceanRequestError({
-            httpStatusCode: res.status,
-            message: resText
-        });
-
-        /** @todo improve types */
-        const data = JSON.parse(resText) as any;
-        return data as Promise<MetOceanPointResponse<T>>;
+        const url = MetOcean.POINT_BASE_URL;
+        const res = await this._makeRequest(url, "post", args, fetchOptions);
+        return res as MetOceanPointResponse<T>;
     }
 
     /**
@@ -474,39 +387,17 @@ export class MetOcean {
      * console.log(res);
      */
     async getRouteTimeSeries<T extends MetOceanTimeSeriesVariable[]>(
-        { route, variables }: MetOceanRouteTimeSeriesArgs<T>,
+        args: MetOceanRouteTimeSeriesArgs<T>,
         fetchOptions?: MetOceanFetchOptions
     ): Promise<MetOceanRouteResponse<T>> {
 
-        if (this._options.validateArgs) {
-            MetOcean._THROW_IF_ROUTE_BAD({ route });
-            MetOcean._THROW_IF_VARIABLES_BAD({ variables });
-        }
+        const url = MetOcean.ROUTE_TIME_SERIES_BASE_URL;
+        const res: any = await this._makeRequest(url, "post", args, fetchOptions);
 
-        //build request
-        const url = MetOcean._ROUTE_TIME_SERIES_BASE_URL;
-        const reqOptions: RequestInit = {
-            ...fetchOptions,
-            method: 'post',
-            headers: this._requestHeaders,
-            body: JSON.stringify({ route, variables }),
-        };
-
-
-        //fetch
-        const res = await fetch(url, reqOptions);
-        const resText = await res.text();
-
-        //Throw request error on bad status code
-        if (res.status !== 200) throw new MetOceanRequestError({
-            httpStatusCode: res.status,
-            message: resText
-        });
-
-        /** @todo improve types */
-        const data = JSON.parse(resText) as any;
-        data.dimensions.timepoint.data = data.dimensions.timepoint.data.map((timepoint: any) => ({ ...timepoint, time: new Date(timepoint.time) }))
-        return data as Promise<MetOceanRouteResponse<T>>;
+        //convert strings to JS Dates
+        res.dimensions.timepoint.data = res.dimensions.timepoint.data
+            .map((timepoint: any) => ({ ...timepoint, time: new Date(timepoint.time) }));
+        return res as Promise<MetOceanRouteResponse<T>>;
     }
     /**
      * Warning: experimental.
@@ -543,40 +434,14 @@ export class MetOcean {
      * console.log(res);
      */
     async getRouteSpeed<T extends MetOceanTimeSeriesVariable[]>(
-        { start, points, speeds, variables }: MetOceanRouteSpeedArgs<T>,
+        args: MetOceanRouteSpeedArgs<T>,
         fetchOptions?: MetOceanFetchOptions
     ): Promise<MetOceanRouteResponse<T>> {
+        const url = MetOcean.ROUTE_SPEED_BASE_URL;
+        const res: any = await this._makeRequest(url, "post", args, fetchOptions);
 
-        if (this._options.validateArgs) {
-            if (!(start instanceof Date)) throw new MetOceanIllegalArgumentError(`property 'start' in getRouteSpeed needs to be an instance of Date, but it was not. It is of type ${typeof start}`)
-            MetOcean._THROW_IF_POINTS_BAD({ points });
-            MetOcean._THROW_IF_SPEEDS_BAD({ speeds });
-            MetOcean._THROW_IF_VARIABLES_BAD({ variables });
-        }
-
-        //build request
-        const url = MetOcean._ROUTE_SPEED_BASE_URL;
-        const reqOptions: RequestInit = {
-            ...fetchOptions,
-            method: 'post',
-            headers: this._requestHeaders,
-            body: JSON.stringify({ start, points, speeds, variables }),
-        };
-
-
-        //fetch
-        const res = await fetch(url, reqOptions);
-        const resText = await res.text();
-
-        //Throw request error on bad status code
-        if (res.status !== 200) throw new MetOceanRequestError({
-            httpStatusCode: res.status,
-            message: resText
-        });
-
-        /** @todo improve types */
-        const data = JSON.parse(resText) as any;
-        data.dimensions.timepoint.data = data.dimensions.timepoint.data.map((timepoint: any) => ({ ...timepoint, time: new Date(timepoint.time) }))
-        return data as Promise<MetOceanRouteResponse<T>>;
+        res.dimensions.timepoint.data = res.dimensions.timepoint.data
+            .map((timepoint: any) => ({ ...timepoint, time: new Date(timepoint.time) }));
+        return res as Promise<MetOceanRouteResponse<T>>;
     }
 }
